26f71a2cfea641be7b5e767badd3872aef143395
Pavdev3 <pnunfe@gmail.com>
2024-01-12 23:41:01 +0100

    Add empty heading element

--- src/app/home/body/current.component.ts
+++ src/app/home/body/current.component.ts
@@ -10,6 +10,7 @@ import { MRData } from '../../Service/interface/formulaApi';
     <p>Temporada: {{ MRData.RaceTable.season }}</p>
     <p>Gran Premio: {{ MRData.RaceTable.Races[0].raceName }}</p>
     <p>Circuito: {{ MRData.RaceTable.Races[0].Circuit.circuitName }}</p>
+    <h2></h2>
   `,
 })
 export class CurrentComponent {
