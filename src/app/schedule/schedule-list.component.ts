f0c6ba04187019d305b881549fdf5f74d0999edb
Pavdev3 <pnunfe@gmail.com>
2024-01-22 20:03:10 +0100

    Refactor schedule-list.component.ts to improve readability and add table layout

--- src/app/schedule/schedule-list.component.ts
+++ src/app/schedule/schedule-list.component.ts
@@ -6,22 +6,69 @@ import { ScheduleService } from './data-access/schedule.service';
   standalone: true,
   selector: 'app-schedule-list',
   imports: [RouterLink],
-  template: `<h1>Schedule</h1>
+  template: `
+    <div class="container text-center">
+      <h1>Schedule</h1>
+      <h2>Season {{ scheduleService.season() }}</h2>
 
-    <h2>Season {{ scheduleService.season() }}</h2>
-    <ul>
-      @for (races of scheduleService.races(); track races.raceName) {
-      <li>
-        <b>{{ races.raceName }}</b> {{ races.date }}
-      </li>
-      <p>
-        <b>P1</b>: {{ races.FirstPractice.time }} <b>Q:</b
-        >{{ races.Qualifying.time }} <b>R:</b>{{ races.time }}
-      </p>
+      <table class="table">
+        <thead>
+          <tr>
+            <th>Race</th>
+            <th>Date</th>
+            <th>Practice 1</th>
+            <th>Qualifying</th>
+            <th>Race Time</th>
+          </tr>
+        </thead>
+        <tbody>
+          @for (races of scheduleService.races(); track races.raceName) {
+          <tr>
+            <td>
+              <b>{{ races.raceName }}</b>
+            </td>
+            <td>{{ races.date }}</td>
+            <td>{{ races.FirstPractice.time }}</td>
+            <td>{{ races.Qualifying.time }}</td>
+            <td>{{ races.time }}</td>
+          </tr>
+          }
+        </tbody>
+      </table>
+
+      <button routerLink="/home">Home</button>
+    </div>
+  `,
+  styles: [
+    `
+      .container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
       }
-    </ul>
 
-    <button routerLink="/home">Home</button>`,
+      table {
+        width: 80%;
+        margin-top: 20px;
+        border-collapse: collapse;
+      }
+
+      th,
+      td {
+        border: 1px solid #ddd;
+        padding: 8px;
+        text-align: left;
+      }
+
+      th {
+        background-color: #f2f2f2;
+      }
+
+      button {
+        margin-top: 20px;
+      }
+    `,
+  ],
 })
 export class ScheduleListComponent {
   scheduleService = inject(ScheduleService);
