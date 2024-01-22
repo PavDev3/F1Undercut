5b764c466ca57bce65847c055b955ae13fc4c798
Pavdev3 <pnunfe@gmail.com>
2024-01-22 19:25:45 +0100

    Refactor standings-list.component.ts

--- src/app/standings/standings-list.component.ts
+++ src/app/standings/standings-list.component.ts
@@ -7,38 +7,73 @@ import { StandingsService } from './data-access/standings-drivers.service';
   standalone: true,
   selector: 'app-standing-list',
   imports: [RouterLink],
-  template: `<h1>Standings</h1>
-    <h2>Season {{ standingsService.season() }}</h2>
+  template: `
+    <div class="container text-center">
+      <div>
+        <h1>Standings</h1>
+        <h2>Season {{ standingsService.season() }}</h2>
+      </div>
+      <div class="row">
+        <div class="col-md-6">
+          <h3>Drivers</h3>
+          <ul>
+            @for (standingsList of standingsService.StandingsLists(); track
+            standingsList.DriverStandings){ @for (Driver of
+            standingsList.DriverStandings; track Driver) {
+            <li>
+              {{ Driver.position }}
+              {{ Driver.Driver.givenName }}
+              {{ Driver.Driver.familyName }}:
+              {{ Driver.points }}
+            </li>
+            } }
+          </ul>
+        </div>
 
-    <h2>Drivers</h2>
-    <ul>
-      @for (standingsList of standingsService.StandingsLists(); track
-      standingsList.DriverStandings){ @for (Driver of
-      standingsList.DriverStandings; track Driver) {
-      <li>
-        {{ Driver.position }}
-        {{ Driver.Driver.givenName }}
-        {{ Driver.Driver.familyName }}
-        {{ Driver.points }}
-      </li>
-      } }
-    </ul>
+        <div class="col-md-6">
+          <h3>Constructors</h3>
+          <ul>
+            @for (standingsList of standingsConstructorService.StandingsLists();
+            track standingsConstructorService.StandingsLists){ @for(Constructor
+            of standingsList.ConstructorStandings ; track Constructor) {
+            <li>
+              {{ Constructor.position }}
+              {{ Constructor.Constructor.name }}:
+              {{ Constructor.points }}
+            </li>
+            } }
+          </ul>
+        </div>
+      </div>
 
-    <h2>Constructors</h2>
+      <button routerLink="/home">Home</button>
+    </div>
+  `,
+  styles: [
+    `
+      .container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+      }
 
-    <ul>
-      @for (standingsList of standingsConstructorService.StandingsLists(); track
-      standingsConstructorService.StandingsLists){ @for(Constructor of
-      standingsList.ConstructorStandings ; track Constructor) {
-      <li>
-        {{ Constructor.position }}
-        {{ Constructor.Constructor.name }}
-        {{ Constructor.points }}
-      </li>
-      } }
-    </ul>
+      .row {
+        display: flex;
+        justify-content: space-around;
+        margin-bottom: 20px;
+      }
 
-    <button routerLink="/home">Home</button> `,
+      .col-md-6 {
+        width: 50%;
+        box-sizing: border-box;
+        padding: 0 50px;
+      }
+
+      button {
+        margin-top: 20px;
+      }
+    `,
+  ],
 })
 export class StandingsListComponent {
   standingsService = inject(StandingsService);
