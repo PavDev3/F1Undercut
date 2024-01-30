c24260aa3d175b7c48f267f49db2ad8da496700f
Pavdev3 <pnunfe@gmail.com>
2024-01-30 15:22:32 +0100

    font added

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { DriversListComponent } from '../drivers/list/drivers-list.component';
 import { ScheduleListComponent } from '../schedule/schedule-list.component';
@@ -16,18 +17,19 @@ import { TeamsListComponent } from '../teams/teams-list.component';
     ScheduleListComponent,
     TeamsListComponent,
     HeaderComponent,
+    MatButtonModule,
   ],
 
   template: `
     <header>
       <div>
         <h1>F1 Undercut</h1>
-        <button routerLink="/last-results">Last Results</button>
-        <button routerLink="/standings">Standings</button>
-        <button routerLink="/schedule">Schedule</button>
-        <button routerLink="/drivers">Drivers</button>
-        <button routerLink="/teams">Teams</button>
-        <button routerLink="/tracks">Tracks</button>
+        <button mat-flat-button routerLink="/last-results">Last Results</button>
+        <button mat-flat-button routerLink="/standings">Standings</button>
+        <button mat-flat-button routerLink="/schedule">Schedule</button>
+        <button mat-flat-button routerLink="/drivers">Drivers</button>
+        <button mat-flat-button routerLink="/teams">Teams</button>
+        <button mat-flat-button routerLink="/tracks">Tracks</button>
       </div>
     </header>
   `,
@@ -37,13 +39,15 @@ import { TeamsListComponent } from '../teams/teams-list.component';
         display: flex;
         justify-content: space-around;
         align-items: center;
-        background-color: #333;
-        color: #fff;
+        background: grey;
         padding: 10px;
       }
       h1 {
         display: flex;
         justify-content: space-around;
+      }
+      button {
+        margin: 8px;
       }
     `,
   ],
