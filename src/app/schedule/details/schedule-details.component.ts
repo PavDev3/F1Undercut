fac6e24080111c6110e2295ba8eedbe031abe95b
Pavdev3 <pnunfe@gmail.com>
2024-01-31 19:38:04 +0100

    Schedule-detail component

---
+++ src/app/schedule/details/schedule-details.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'schedule-details',
+  template: ` <h1>Schedule Details</h1> `,
+})
+export class ScheduleDetailComponent {}
