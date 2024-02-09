2730dfb431e16d6b55a4d5fb02dbec3bd4806a12
Pavdev3 <pnunfe@gmail.com>
2024-02-09 13:11:41 +0100

    media query

--- src/app/drivers/list/drivers-list.component.ts
+++ src/app/drivers/list/drivers-list.component.ts
@@ -10,35 +10,37 @@ import { DriversService } from './data-access/drivers.service';
     <div class="container">
       <h1>Driver List</h1>
       <h2>Season {{ driversService.season() }}</h2>
-      <table class="driverList">
-        <thead>
-          <tr>
-            <th>Number</th>
-            <th>Driver</th>
-            <th>Nationality</th>
-          </tr>
-        </thead>
-        <tbody>
-          @for (driver of driversService.drivers(); track driver.driverId) {
-          <tr>
-            <td>{{ driver.permanentNumber }}</td>
-            <td>
-              <a [routerLink]="['/driver-details', driver.driverId]">
-                {{ driver.givenName }} {{ driver.familyName }}
-              </a>
-            </td>
-            <td>
-              <div
-                class="fflag ff-md {{
-                  'fflag-' + driver.nationality.replaceAll(' ', '')
-                }} "
-              ></div>
-              {{ driver.nationality }}
-            </td>
-          </tr>
-          }
-        </tbody>
-      </table>
+      <div class="table-wrapper">
+        <table class="driverList">
+          <thead>
+            <tr>
+              <th>Number</th>
+              <th>Driver</th>
+              <th>Nationality</th>
+            </tr>
+          </thead>
+          <tbody>
+            @for (driver of driversService.drivers(); track driver.driverId) {
+            <tr>
+              <td>{{ driver.permanentNumber }}</td>
+              <td>
+                <a [routerLink]="['/driver-details', driver.driverId]">
+                  {{ driver.givenName }} {{ driver.familyName }}
+                </a>
+              </td>
+              <td>
+                <div
+                  class="fflag ff-md {{
+                    'fflag-' + driver.nationality.replaceAll(' ', '')
+                  }} "
+                ></div>
+                {{ driver.nationality }}
+              </td>
+            </tr>
+            }
+          </tbody>
+        </table>
+      </div>
     </div>
   `,
   styles: [
@@ -49,7 +51,7 @@ import { DriversService } from './data-access/drivers.service';
         transition: color 0.3s;
       }
       a:hover {
-        color: blue; /* Cambia el color del texto cuando se pasa el cursor sobre el enlace */
+        color: blue;
       }
       .container {
         display: flex;
@@ -57,8 +59,13 @@ import { DriversService } from './data-access/drivers.service';
         align-items: center;
       }
 
+      .table-wrapper {
+        overflow-x: auto;
+        width: 100%;
+      }
+
       table {
-        width: 45%;
+        width: 100%;
         margin-top: 20px;
         border-collapse: collapse;
       }
@@ -72,6 +79,13 @@ import { DriversService } from './data-access/drivers.service';
 
       th {
         background-color: #f2f2f2;
+      }
+
+      @media (max-width: 767px) {
+        th:nth-child(4),
+        td:nth-child(4) {
+          display: none;
+        }
       }
     `,
   ],
