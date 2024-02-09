aec95c3593f8612e075c46672fc8d888b31719c2
Pavdev3 <pnunfe@gmail.com>
2024-02-09 17:33:11 +0100

    Remove entry for environments folder in .gitignore

---
+++ environments/environment.ts
@@ -0,0 +1,14 @@
+export const _currentResult =
+  'https://ergast.com/api/f1/current/last/results.json';
+
+export const _driversBySeason = 'https://ergast.com/api/f1/2023/drivers.json';
+export const _tracksBySeason = 'https://ergast.com/api/f1/2023/circuits.json';
+export const _constructorsBySeason =
+  'https://ergast.com/api/f1/2023/constructors.json';
+
+export const _scheduleBySeason = 'https://ergast.com/api/f1/2023.json';
+export const _standingsDriversBySeason =
+  'https://ergast.com/api/f1/current/driverStandings.json';
+export const _standingsConstructorsBySeason =
+  'https://ergast.com/api/f1/current/constructorStandings.json';
+export const _scheduleDetailsResults = 'https://ergast.com/api/f1/2023/';
