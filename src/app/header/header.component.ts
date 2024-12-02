d94c8988d1ab3d8787c1c24344a2adffab8fcc88
pablo <pnunfe@gmail.com>
2024-12-02 10:24:12 +0100

    update UI and header to Spanish

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,24 +1,11 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { RouterLink, RouterOutlet } from '@angular/router';
-import { DriversListComponent } from '../drivers/list/drivers-list.component';
-import { ScheduleListComponent } from '../schedule/list/schedule-list.component';
-import { StandingsListComponent } from '../standings/standings-list.component';
-import { TeamsListComponent } from '../teams/list/teams-list.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   standalone: true,
   selector: 'app-header',
-  imports: [
-    RouterOutlet,
-    RouterLink,
-    DriversListComponent,
-    StandingsListComponent,
-    ScheduleListComponent,
-    TeamsListComponent,
-    HeaderComponent,
-    MatButtonModule,
-  ],
+  imports: [RouterLink, MatButtonModule],
 
   template: `
     <header>
@@ -26,12 +13,14 @@ import { TeamsListComponent } from '../teams/list/teams-list.component';
         <a [routerLink]="['']">
           <h1>>>F1 Undercut<<</h1>
         </a>
-        <button mat-flat-button routerLink="/last-results">Last Results</button>
-        <button mat-flat-button routerLink="/standings">Standings</button>
-        <button mat-flat-button routerLink="/schedule">Schedule</button>
-        <button mat-flat-button routerLink="/drivers">Drivers</button>
-        <button mat-flat-button routerLink="/teams">Teams</button>
-        <button mat-flat-button routerLink="/tracks">Tracks</button>
+        <button mat-flat-button routerLink="/last-results">
+          Últimos Resultados
+        </button>
+        <button mat-flat-button routerLink="/standings">Clasificación</button>
+        <button mat-flat-button routerLink="/schedule">Calendario</button>
+        <button mat-flat-button routerLink="/drivers">Pilotos</button>
+        <button mat-flat-button routerLink="/teams">Escuderías</button>
+        <button mat-flat-button routerLink="/tracks">Circuitos</button>
       </div>
     </header>
   `,
